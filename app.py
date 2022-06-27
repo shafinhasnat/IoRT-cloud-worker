997805cdd190a3bd05b47ec5cb3c3a89d25e3814
shafinhasnat <shafinhasnat97@gmail.com>
2022-06-27 18:08:31 +0600

    v0.0.4

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from pydoc import cli
 import paho.mqtt.client as mqtt
 import json
 import os
+import time
 
 client=mqtt.Client()
-IP = os.environ.get("IP")
-PORT = os.environ.get("PORT")
+IP = "36.255.69.54"
+PORT = 1883
 client.connect(IP, PORT, 60)
+
+latency = []
 
 def on_connect(client,userdata,flags,rc):
      print('Connected with result code:'+str(rc))
@@ -15,18 +18,21 @@ def on_connect(client,userdata,flags,rc):
 
 def on_message(_, userdata, msg):
     data = msg.payload.decode('utf-8')
-    bot1 = json.loads(data)["bot1"]
-    bot2 = json.loads(data)["bot2"]
-    if bot1>=bot2:
-        print("Task allocated to bot2")
-        client.publish("bot2", bot2)
-    else:
+    print("***************")
+    print(json.loads(data), '\nTime in system:', time.time(), '\nLatency:',float(time.time()) - float(json.loads(data).get('timestamp')))
+    bot1 = json.loads(data).get("bot1")
+    bot2 = json.loads(data).get("bot2")
+
+    if bot1<=bot2:
         print("Task allocated to bot1")
-        client.publish("bot1", bot1)
-
-
+        client.publish("bot1", "f")
+    else:
+        print("Task allocated to bot2")
+        client.publish("bot2", "f")
+    # latency.append(float(time.time()) - float(json.loads(data).get('timestamp')))
+    # print(latency)
+    print("***************\n")
 
 client.on_connect=on_connect
 client.on_message=on_message
-
 client.loop_forever()
